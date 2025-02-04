fe8a34701d17f6c4dc97c5c182e2d1ca837d590a
Genta Takahashi <genta.t07092@gmail.com>
2025-02-03 23:52:49 -0600

    drought 1023 WIP

--- 1023.py
+++ 1023.py
@@ -1,15 +1,31 @@
-def city_data():
+from collections import defaultdict
+
+def city_data(n):
+    
+    data_dict = defaultdict(int) # defaultdictかなんちゃらをここで作って、理想的な辞書をつくる
     totalnum_people = 0
     total_comsumption = 0
     
-    num_people, consumption = int(input().split())
-    totalnum_people += num_people
-    total_comsumption += consumption
+    while n > 0:
+        
+        num_people, consumption = int(input().split())
+        
+        consumption_per_people = consumption // num_people
+        totalnum_people += num_people
+        total_comsumption += consumption
+        
+        data_dict[consumption_per_people] += num_people
+        
+        n += -1
+        
+    return total_comsumption/totalnum_people, data_dict # consumo medio and data to print
     
     
     
 def main():
-    return 0
+    num_properties = int(input())
+    avg_consumption_per_people, data_dict = city_data(num_properties)
+    
 
 
 if __name__ == "(__main__)":
