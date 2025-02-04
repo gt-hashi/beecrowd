bbba515d94713afa93417448e3968f098b9d76f7
Genta Takahashi <genta.t07092@gmail.com>
2025-02-03 23:52:26 -0600

    Drought 1023 WIP

---
+++ 1023.py
@@ -0,0 +1,16 @@
+def city_data():
+    totalnum_people = 0
+    total_comsumption = 0
+    
+    num_people, consumption = int(input().split())
+    totalnum_people += num_people
+    total_comsumption += consumption
+    
+    
+    
+def main():
+    return 0
+
+
+if __name__ == "(__main__)":
+    main()
